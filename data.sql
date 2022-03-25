e3f64e1f62b5b7dbecd7e7dee6c0ce898811ec21
Aime Malaika <aimemalaika1995@gmail.com>
2022-03-25 16:10:44 +0200

    Exercise to query multiple tables

--- data.sql
+++ data.sql
@@ -16,3 +16,30 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 ('Boarmon', 'Jun 7 2005', 7, TRUE, 20.4)
 ('Blossom', 'Oct 13 1998', 3, TRUE, 17)
 ('Ditto', 'May 14 2022', 4, TRUE, 22);
+
+INSERT INTO owners(full_name, age)VALUES 
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species(name) VALUES 
+('Pokemon'),
+('Digimon');
+
+-- Modify your inserted animals so it includes the species_id value: 
+UPDATE animals SET species_id=2 where name LIKE '%mon';
+UPDATE animals SET species_id=1 where name NOT LIKE '%mon';
+
+-- Modify your inserted animals to include owner information (owner_id): 
+UPDATE animals SET owners_id=1 WHERE name='Agumon';
+UPDATE animals SET owners_id=2 where name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owners_id=3 where name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owners_id=4 where name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owners_id=5 where name IN ('Angemon', 'Boarmon');
+
+-- Update owners names with typo in Local DB
+UPDATE owners SET full_name='Dean Winchester' WHERE full_name like 'Dean Wincheste';
+UPDATE owners SET full_name='Jodie Whittaker' WHERE full_name like 'Jodie Whittake'; 
--- schema.sql
+++ schema.sql
@@ -17,3 +17,33 @@ ALTER TABLE animals ADD species VARCHAR(50) DEFAULT NULL;
 
 
 
+CREATE TABLE owners (
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    full_name varchar(100),
+    age integer
+);
+
+CREATE TABLE species (
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar(100)
+);
+
+-- Modify animals table: 
+-- Make sure that id is set as autoincremented PRIMARY KEY
+ALTER TABLE animals
+ADD CONSTRAINT animals_pkey PRIMARY KEY (id);
+
+-- Remove column species
+ALTER TABLE animals
+DROP COLUMN species;
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals
+ADD species_id integer;
+
+ALTER TABLE animals 
+ADD FOREIGN KEY (species_id) REFERENCES species (id);
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals
+ADD owners_id integer;
+ALTER TABLE animals 
+ADD FOREIGN KEY (owners_id) REFERENCES owners (id);
